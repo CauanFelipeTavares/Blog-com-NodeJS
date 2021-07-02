697f200de5129ef0da8d55a5f9bf402b1ef988fc
Cauan Felipe Tavares <tavarescauanf@gmail.com>
2021-07-02 01:02:46 -0300

    40 aulas completas

--- index.js
+++ index.js
@@ -2,35 +2,70 @@
 
     const express = require('express')
     const handlebars = require('express-handlebars')
-    //const bodyParser = require('body-parser')
     const router = require('./routes/admin')
-    //const mongoose = require('mongoose')
+    const mongoose = require('mongoose')
     const app = express()
     const admin = require('./routes/admin')
     const path = require('path')
-
+    const session = require('express-session')
+    const flash = require('connect-flash')
 
 //Configurações
-    //app.use(bodyParser.urlencoded({extended = true}));
-    //app.use(bodyParser.json());
+
+//Session & Flash
+    app.use(session({
+        secret: 'senha',
+        resave: true,
+        saveUninitialized: true
+    }))
+    app.use(flash())
+
+    app.use((req,res,next) => {
+        res.locals.success_msg = req.flash("success_msg")
+        res.locals.error_msg = req.flash("error_msg")
+        next()
+    })
+
+//Body Parser
+    app.use(express.json()) 
+    app.use(express.urlencoded({extended: true}))
 
     app.engine('handlebars', handlebars({defaultLayout: 'main'}))
     app.set('view engine', 'handlebars')
 
+//Banco de Dados
+    mongoose.Promise = global.Promise
+    mongoose.connect("mongodb://localhost/blogapp", {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    }).then(() => {
+        console.log("Conectado com o MongoDb")
+    }).catch((erro) => {
+        console.log("Erro ao conectar com o banco de dados MongoDb. " + erro)
+    })
+
+
+//Arquivos Estáticos (HTML,CSS)
     app.use(express.static(path.join(__dirname + '/public')))
+
 //Rotas
+
+//Rota Principal
     app.get('/', (req,res) => {
         res.send("Pagina principal, adicionar futuramente um layout")
     })
 
+//Rotas Admin
     app.use('/admin', admin)
 
-
 //Outros
+
+//Rodar em LocalHost
     const port = 8181
     app.listen(port,() => {
         console.log("Servidor rodando em localhost:" + port)
     })
+    
 /*
 localhost:8081
 */
--- routes/admin.js
+++ routes/admin.js
@@ -1,5 +1,8 @@
 const express = require('express')
 const router = express.Router()
+const mongoose = require('mongoose')
+require('../models/Categoria')
+const categoria = mongoose.model("categorias")
 
     router.get('/home', (req,res) => {
         res.send("Pagina principal do adm fml")
@@ -9,11 +12,56 @@ const router = express.Router()
     })
 
     router.get('/categorias', (req,res) => {
-        res.render('admin/categorias')
+        categoria.find().sort({date:'desc'}).then((categorias) => {
+            res.render('admin/categorias', {categorias: categorias.map(categoria =>
+                categoria.toJSON()
+            )})
+        }).catch((erro) => {
+            req.flash("error_msg", "Houve um erro ao listar as categorias")
+            res.render('/admin')
+        })
+        
     })
 
     router.get('/categorias/add', (req,res) => {
         res.render('admin/addcategoria')
     })
+
+    router.post('/categorias/nova', (req,res) => {
+
+        var erros = []
+
+        if(!req.body.nome || typeof req.body.nome == undefined || req.body.nome == null){
+            erros.push({texto: "Nome Inválido"})
+        }
+        if(!req.body.slug || typeof req.body.slug == undefined || req.body.slug == null){
+            erros.push({texto: "Slug Inválido"})
+        }
+        if(req.body.nome.length < 2){
+            erros.push({texto: "Nome da categoria pequeno demais"})
+        }
+        if(req.body.slug.length < 3){
+            erros.push({texto: "Slug da categoria pequeno demais"})
+        }
+
+        if(erros.length > 0){
+            res.render('admin/addcategoria', {erros: erros})
+        }else{
+            const novaCategoria =  {
+                nome: req.body.nome,
+                slug: req.body.slug
+            }
+    
+            new categoria(novaCategoria).save().then(() => {
+                req.flash('success_msg', "Categoria criada com sucesso!")
+                res.redirect('/admin/categorias')
+            }).catch((erro) => {
+                req.flash('error_msg', "Erro ao salvar a categoria. Tente novamente.")
+                res.redirect('/admin')
+            })
+        }
+
+    })
+
 
 module.exports = router
